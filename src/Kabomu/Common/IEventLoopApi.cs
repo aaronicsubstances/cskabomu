cfc189f00f2d21ff38f1615434915cf6a2553516
Aaron Baffour-Awuah <aaronbaffourawuah@gmail.com>
2022-04-30 07:15:36 -0400

    Added mutual exclusion api to help offload concurrency decisions to library clients.
    Simplified DefaultQuasiHttpApplication under assumption that async/await will be used for concurrency.

--- src/Kabomu/Common/IEventLoopApi.cs
+++ src/Kabomu/Common/IEventLoopApi.cs
@@ -4,16 +4,15 @@ using System.Text;
 
 namespace Kabomu.Common
 {
-    public interface IEventLoopApi
+    public interface IEventLoopApi : IMutexApi
     {
         long CurrentTimestamp { get; }
-        void PostCallback(Action<object> cb, object cbState);
         object ScheduleTimeout(int millis, Action<object> cb, object cbState);
         void CancelTimeout(object id);
         UncaughtErrorCallback ErrorHandler { get; set; }
         bool IsEventDispatchThread { get; }
-        bool IsSuperiorPeriodicTimeoutAvailable { get; }
+        /*bool IsSuperiorPeriodicTimeoutAvailable { get; }
         object SchedulePeriodicTimeout(int millis, Action<object> cb, object cbState);
-        void CancelPeriodicTimeout(object id);
+        void CancelPeriodicTimeout(object id);*/
     }
 }
--- src/Kabomu/QuasiHttp/Bodies/ByteBufferBody.cs
+++ src/Kabomu/QuasiHttp/Bodies/ByteBufferBody.cs
@@ -9,29 +9,25 @@ namespace Kabomu.QuasiHttp.Bodies
     {
         private Exception _srcEndError;
 
-        public ByteBufferBody(byte[] data)
-            : this(data, 0, data?.Length ?? 0, "application/octet-stream")
-        { }
-
-        public ByteBufferBody(byte[] data, int offset, int length, string contentType)
+        public ByteBufferBody(byte[] data, int offset, int length, string contentType, IMutexApi mutexApi)
         {
             if (!ByteUtils.IsValidMessagePayload(data, offset, length))
             {
                 throw new ArgumentException("invalid buffer");
             }
+
             Buffer = data;
             Offset = offset;
             ContentLength = length;
             ContentType = contentType;
+            MutexApi = mutexApi ?? new BlockingMutexApi(this);
         }
 
         public byte[] Buffer { get; }
-
         public int Offset { get; }
-
-        public string ContentType { get; }
-
         public int ContentLength { get; }
+        public string ContentType { get; }
+        public IMutexApi MutexApi { get; }
 
         public void OnDataRead(QuasiHttpBodyCallback cb)
         {
@@ -39,21 +35,27 @@ namespace Kabomu.QuasiHttp.Bodies
             {
                 throw new ArgumentException("null callback");
             }
-            if (_srcEndError != null)
+            MutexApi.PostCallback(_ =>
             {
-                cb.Invoke(_srcEndError, null, 0, 0, false);
-                return;
-            }
-            cb.Invoke(null, Buffer, Offset, ContentLength, false);
+                if (_srcEndError != null)
+                {
+                    cb.Invoke(_srcEndError, null, 0, 0, false);
+                    return;
+                }
+                cb.Invoke(null, Buffer, Offset, ContentLength, false);
+            }, null);
         }
 
         public void Close()
         {
-            if (_srcEndError != null)
+            MutexApi.PostCallback(_ =>
             {
-                return;
-            }
-            _srcEndError = new Exception("closed");
+                if (_srcEndError != null)
+                {
+                    return;
+                }
+                _srcEndError = new Exception("closed");
+            }, null);
         }
     }
 }
