7420fe83eb8ff84a4aea63db445fc8b366ba375b
Aaron Baffour-Awuah <aaron.baffour-awuah@opiesoftware.com>
2022-06-01 11:02:04 -0400

    Added test for full reading of quasi http bodies. Removed unneeded cancellation token source code in Program.cs files in example projects.

--- examples/Udp.FileClient/Program.cs
+++ examples/Udp.FileClient/Program.cs
@@ -6,7 +6,6 @@ using NLog;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Udp.FileClient
@@ -50,7 +49,6 @@ namespace Udp.FileClient
                     LOG.Error("Event Loop error! {0}: {1}", m, e);
                 }
             };
-            var stopHandle = new CancellationTokenSource();
             var udpTransport = new LocalhostUdpTransport(port)
             {
                 EventLoop = eventLoop,
