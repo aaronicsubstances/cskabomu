8ead4c67b64b9ffa59ec53047ab685b25df6c3af
Aaron Baffour-Awuah <aaron.baffour-awuah@opiesoftware.com>
2022-07-07 11:03:09 -0400

    Added test for lock based mutex api factory.

---
+++ test/Kabomu.Tests/Concurrency/LockBasedMutexApiFactoryTest.cs
@@ -0,0 +1,20 @@
+﻿using Kabomu.Concurrency;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kabomu.Tests.Concurrency
+{
+    public class LockBasedMutexApiFactoryTest
+    {
+        [Fact]
+        public async Task TestCreate()
+        {
+            var instance = new LockBasedMutexApiFactory();
+            var mutex = await instance.Create();
+            Assert.True(mutex is LockBasedMutexApi);
+        }
+    }
+}
