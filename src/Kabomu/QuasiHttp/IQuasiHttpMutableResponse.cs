2b4e5f9db2477017c3e535f61e97a39631629f6e
Aaron Baffour-Awuah <aaronbaffourawuah@gmail.com>
2022-08-09 09:30:28 -0400

    Updated interface type of response headers in new mutable response interface.

--- src/Kabomu/QuasiHttp/IQuasiHttpMutableResponse.cs
+++ src/Kabomu/QuasiHttp/IQuasiHttpMutableResponse.cs
@@ -5,6 +5,10 @@ using System.Text;
 
 namespace Kabomu.QuasiHttp
 {
+    /// <summary>
+    /// Provides convenient extension of <see cref="IQuasiHttpResponse"/> interface
+    /// in which properties of the interface are mutable.
+    /// </summary>
     public interface IQuasiHttpMutableResponse : IQuasiHttpResponse
     {
         new bool StatusIndicatesSuccess { get; set; }
@@ -13,7 +17,7 @@ namespace Kabomu.QuasiHttp
 
         new string StatusMessage { get; set; }
 
-        new IDictionary<string, List<string>> Headers { get; set; }
+        new IDictionary<string, IList<string>> Headers { get; set; }
 
         new IQuasiHttpBody Body { get; set; }
 
