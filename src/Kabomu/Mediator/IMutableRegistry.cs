5b8df4a0c9e3dabef2c575ead26a7cffa1d34f4d
Aaron Baffour-Awuah <aaron.baffour-awuah@opiesoftware.com>
2022-08-11 06:21:37 -0400

    Updated interfaces required by Kabomu.Mediator quasi web framework. Added RegistryUtils to ease burden of implementing IRegistry.

--- src/Kabomu/Mediator/IMutableRegistry.cs
+++ src/Kabomu/Mediator/IMutableRegistry.cs
@@ -1,20 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Kabomu.Mediator
 {
     public interface IMutableRegistry : IRegistry
     {
-        Task Add(string key, object value);
-        Task Add(Type t, object value);
-        Task AddByType<T>(T value);
-        Task AddLazy(string key, Func<Task<object>> valueGenerator);
-        Task AddLazy(Type t, Func<Task<object>> valueGenerator);
-        Task AddLazyByType<T>(Func<Task<T>> valueGenerator);
-        Task<bool> TryRemove(string key);
-        Task<bool> TryRemove(Type t);
-        Task<bool> TryRemoveByType<T>();
+        void Add(string key, object value);
+        void Add(Type key, object value);
+        void AddLazy(string key, Func<object> valueGenerator);
+        void AddLazy(Type key, Func<object> valueGenerator);
+        bool Remove(string key);
+        bool Remove(Type key);
     }
 }
---
+++ src/Kabomu/Mediator/RegistryUtils.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kabomu.Mediator
+{
+    public static class RegistryUtils
+    {
+        public static object Get(IRegistry instance, string key)
+        {
+            var (present, value) = instance.TryGet(key);
+            if (!present)
+            {
+                throw new ArgumentException("key not found", nameof(key));
+            }
+            return value;
+        }
+
+        public static object Get(IRegistry instance, Type key)
+        {
+            var (present, value) = instance.TryGet(key);
+            if (!present)
+            {
+                throw new ArgumentException("key not found", nameof(key));
+            }
+            return value;
+        }
+
+        public static object GetFirstNonNull(IRegistry instance, string key, Func<object, object> transformFunction)
+        {
+            return instance.GetAll(key)
+                .Select(x => transformFunction.Invoke(x))
+                .FirstOrDefault(x => x != null);
+        }
+
+        public static object GetFirstNonNull(IRegistry instance, Type key, Func<object, object> transformFunction)
+        {
+            return instance.GetAll(key)
+                .Select(x => transformFunction.Invoke(x))
+                .FirstOrDefault(x => x != null);
+        }
+    }
+}
