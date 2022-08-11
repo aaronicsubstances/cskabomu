aad7d401ba3ce67247745d8ecdedc463d159660a
Aaron Baffour-Awuah <aaronbaffourawuah@gmail.com>
2022-08-11 16:24:02 -0400

    Added default quasi http application based on Kabomu.Mediator quasi web framework. Tested default quasi http application with memory based example.

--- src/Kabomu/Mediator/RequestParsing/NoSuchParserException.cs
+++ src/Kabomu/Mediator/RequestParsing/NoSuchParserException.cs
@@ -3,8 +3,7 @@ using System.Runtime.Serialization;
 
 namespace Kabomu.Mediator.RequestParsing
 {
-    [Serializable]
-    internal class NoSuchParserException : Exception
+    public class NoSuchParserException : Exception
     {
         public NoSuchParserException()
         {
@@ -15,10 +14,6 @@ namespace Kabomu.Mediator.RequestParsing
         }
 
         public NoSuchParserException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
-
-        protected NoSuchParserException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
     }
