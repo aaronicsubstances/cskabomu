12247dd42d3f92731fef2e3e34b3fd52c29fd910
Aaron Baffour-Awuah <aaron.baffour-awuah@opiesoftware.com>
2022-08-12 08:50:40 -0400

    Simplified registry by replacing concept of values sources with value generators in which "lazy" is just one of many possible x'tic.

--- src/Kabomu/Mediator/Handling/DefaultContext.cs
+++ src/Kabomu/Mediator/Handling/DefaultContext.cs
@@ -21,7 +21,7 @@ namespace Kabomu.Mediator.Handling
         public IList<Handler> InitialHandlers { get; set; }
         public Handler FinalHandler { get; set; }
 
-        public async Task Start()
+        public Task Start()
         {
             _handlerStack = new Stack<HandlerGroup>();
             var firstHandlerGroup = new HandlerGroup(InitialHandlers, InitialReadonlyLocalRegistry);
@@ -29,14 +29,8 @@ namespace Kabomu.Mediator.Handling
 
             _joinedRegistry = new DynamicRegistry(this).Join(ReadonlyGlobalRegistry);
 
-            try
-            {
-                await Next();
-            }
-            catch (Exception e)
-            {
-                await HandleError(e);
-            }
+            _ = RunNext();
+            return Task.CompletedTask;
         }
 
         private IRegistry CurrentRegistry
@@ -47,7 +41,7 @@ namespace Kabomu.Mediator.Handling
             }
         }
 
-        public Task Insert(IList<Handler> handlers)
+        public void Insert(IList<Handler> handlers)
         {
             if ((handlers?.Count ?? 0) == 0)
             {
@@ -56,10 +50,10 @@ namespace Kabomu.Mediator.Handling
 
             var newHandlerGroup = new HandlerGroup(handlers, CurrentRegistry);
             _handlerStack.Push(newHandlerGroup);
-            return Next();
+            _ = RunNext();
         }
 
-        public Task Insert(IRegistry registry, IList<Handler> handlers)
+        public void Insert(IRegistry registry, IList<Handler> handlers)
         {
             if ((handlers?.Count ?? 0) == 0)
             {
@@ -68,50 +62,55 @@ namespace Kabomu.Mediator.Handling
 
             var newHandlerGroup = new HandlerGroup(handlers, CurrentRegistry.Join(registry));
             _handlerStack.Push(newHandlerGroup);
-            return Next();
+            _ = RunNext();
         }
 
-        public Task Next(IRegistry registry)
+        public void Next(IRegistry registry)
         {
             _handlerStack.Peek().registry = CurrentRegistry.Join(registry);
-            return Next();
+            _ = RunNext();
         }
 
-        public async Task Next()
+        public void Next()
         {
-            Handler handler = null;
-            // allow null handlers to be inserted.
-            while (_handlerStack.Count > 0 && handler == null)
-            {
-                var currentHandlerGroup = _handlerStack.Peek();
-                if (currentHandlerGroup.HasNext())
-                {
-                    handler = currentHandlerGroup.Next();
-                }
-                else
-                {
-                    _handlerStack.Pop();
-                }
-            }
+            _ = RunNext();
+        }
 
-            if (handler == null)
-            {
-                handler = FinalHandler;
-            }
-
+        private async Task RunNext()
+        {
             try
             {
+                // tolerate prescence of nulls.
+                Handler handler = null;
+                while (_handlerStack.Count > 0 && handler == null)
+                {
+                    var currentHandlerGroup = _handlerStack.Peek();
+                    if (currentHandlerGroup.HasNext())
+                    {
+                        handler = currentHandlerGroup.Next();
+                    }
+                    else
+                    {
+                        _handlerStack.Pop();
+                    }
+                }
+
+                if (handler == null)
+                {
+                    handler = FinalHandler;
+                }
+
                 await handler.Invoke(this);
             }
             catch (Exception e)
             {
-                if (e is HandlerException)
+                if (!(e is HandlerException))
                 {
-                    throw;
+                    await HandleError(new HandlerException(null, e));
                 }
                 else
                 {
-                    throw new HandlerException("", e);
+                    await HandleError(e);
                 }
             }
         }
@@ -124,52 +123,82 @@ namespace Kabomu.Mediator.Handling
             }
         }
 
-        public Task<T> ParseRequest<T>(object parseOpts)
+        public async Task<T> ParseRequest<T>(object parseOpts)
         {
-            // allow null parsers to be inserted.
-            Func<IRequestParser, (bool, Task<T>)> parserCode = (parser) =>
+            try
             {
-                if (parser != null && parser.CanParse<T>(this, parseOpts))
+                // tolerate prescence of nulls.
+                Func<IRequestParser, (bool, Task<T>)> parserCode = (parser) =>
                 {
-                    var result = parser.Parse<T>(this, parseOpts);
-                    return (true, result);
+                    if (parser != null && parser.CanParse<T>(this, parseOpts))
+                    {
+                        var result = parser.Parse<T>(this, parseOpts);
+                        return (true, result);
+                    }
+                    return (false, null);
+                };
+                var (found, parserTask) = _joinedRegistry.TryGetFirst(parserCode);
+                if (!found)
+                {
+                    throw new ParseException("no parser found");
                 }
-                return (false, null);
-            };
-            var (found, parserTask) = _joinedRegistry.TryGetFirst(parserCode);
-            if (!found)
-            {
-                throw new NoSuchParserException();
+                else
+                {
+                    return await parserTask;
+                }
             }
-            else
+            catch (Exception e)
             {
-                return parserTask;
+                if (e is ParseException)
+                {
+                    throw;
+                }
+                else
+                {
+                    throw new ParseException(null, e);
+                }
             }
         }
 
-        public Task RenderResponse(object body)
+        public async Task RenderResponse(object body)
         {
-            if (body is IRenderable renderable)
+            try
             {
-                return renderable.Render(this);
-            }
-            Func<IRenderer, (bool, Task)> renderingCode = (renderer) =>
-            {
-                if (renderer != null && renderer.CanRender(this, body))
+                if (body is IRenderable renderable)
                 {
-                    var result = renderer.Render(this, body);
-                    return (true, result);
+                    await renderable.Render(this);
+                    return;
                 }
-                return (false, null);
-            };
-            var (found, renderTask) = _joinedRegistry.TryGetFirst(renderingCode);
-            if (!found)
-            {
-                throw new NoSuchRendererException();
+                // tolerate prescence of nulls.
+                Func<IRenderer, (bool, Task)> renderingCode = (renderer) =>
+                {
+                    if (renderer != null && renderer.CanRender(this, body))
+                    {
+                        var result = renderer.Render(this, body);
+                        return (true, result);
+                    }
+                    return (false, null);
+                };
+                var (found, renderTask) = _joinedRegistry.TryGetFirst(renderingCode);
+                if (!found)
+                {
+                    throw new RenderException("no renderer found");
+                }
+                else
+                {
+                    await renderTask;
+                }
             }
-            else
+            catch (Exception e)
             {
-                return renderTask;
+                if (e is RenderException)
+                {
+                    throw;
+                }
+                else
+                {
+                    throw new RenderException(null, e);
+                }
             }
         }
 
@@ -195,20 +224,20 @@ namespace Kabomu.Mediator.Handling
 
         private async Task HandleErrorLastResort(Exception original, Exception errorHandlerException)
         {
-            string msg;
-            if (errorHandlerException != null)
-            {
-                msg = "Exception thrown by error handler while handling exception\n" +
-                    "Original exception: " + FlattenException(original) + "\n" +
-                    "Error handler exception: " + FlattenException(errorHandlerException);
-            }
-            else
-            {
-                msg = FlattenException(original);
-            }
-
             try
             {
+                string msg;
+                if (errorHandlerException != null)
+                {
+                    msg = "Exception thrown by error handler while handling exception\n" +
+                        "Original exception: " + FlattenException(original) + "\n" +
+                        "Error handler exception: " + FlattenException(errorHandlerException);
+                }
+                else
+                {
+                    msg = FlattenException(original);
+                }
+
                 await Response.SetStatusIndicatesSuccess(false)
                     .SetStatusIndicatesClientError(false)
                     .SendWithBody(new StringBody(msg) { ContentType = "text/plain" });
@@ -216,7 +245,7 @@ namespace Kabomu.Mediator.Handling
             catch (Exception) { }
         }
 
-        private static string FlattenException(Exception exception)
+        internal static string FlattenException(Exception exception)
         {
             var stringBuilder = new StringBuilder();
 
