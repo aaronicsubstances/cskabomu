5018d87a6154f974bafde83ffefe5b357d8284ab
Aaron Baffour-Awuah <aaronbaffourawuah@gmail.com>
2022-08-18 08:35:02 -0400

    Updated default path template matching algorithm to cater for edge cases.

--- src/Kabomu/Mediator/Path/DefaultPathTemplateSpecification.cs
+++ src/Kabomu/Mediator/Path/DefaultPathTemplateSpecification.cs
@@ -4,10 +4,8 @@ namespace Kabomu.Mediator.Path
 {
     public class DefaultPathTemplateSpecification
     {
-        public bool CaseSensitiveMatchEnabled { get; set; }
-        public bool? MatchTrailingSlash { get; set; }
         public IDictionary<string, string> DefaultValues { get; set; }
-        public IList<IList<string>> SampleSets { get; set; }
+        public IList<DefaultPathTemplateExample> SampleSets { get; set; }
         public IDictionary<string, string> ConstraintSpecs { get; set; }
     }
 }
--- src/Kabomu/Mediator/Path/DefaultPathToken.cs
+++ src/Kabomu/Mediator/Path/DefaultPathToken.cs
@@ -10,16 +10,54 @@ namespace Kabomu.Mediator.Path
 
         public const int TokenTypeWildCard = 3;
 
-        public int Type;
-        public string Value;
-        public int SampleIndexOfValue = -1;
-        public bool EmptySegmentAllowed = false;
+        public readonly int Type;
 
-        public void UpdateValue(int sampleIndex, string value)
+        public DefaultPathToken(DefaultPathToken src, int overridingType)
         {
-            // only update if incoming sample index precedes the existing sample
-            // in original submission.
-            if (sampleIndex < SampleIndexOfValue)
+            Type = overridingType;
+            if (src == null)
+            {
+                SampleIndexOfValue = -1;
+            }
+            else
+            {
+                Value = src.Value;
+                SampleIndexOfValue = src.SampleIndexOfValue;
+                EmptySegmentAllowed = src.EmptySegmentAllowed;
+            }
+        }
+
+        public string Value { get; private set; }
+        public int SampleIndexOfValue { get; private set; }
+        public bool EmptySegmentAllowed { get; private set; }
+
+        public void Update(int sampleIndex, string value)
+        {
+            if (value == "")
+            {
+                EmptySegmentAllowed = true;
+            }
+            bool proceedWithUpdate = false;
+            if (SampleIndexOfValue == -1)
+            {
+                // first time.
+                proceedWithUpdate = true;
+            }
+            else if (Value == "" && value != "")
+            {
+                // always allow non empty values to override empty ones
+                // even if non empty value appears after empty one in
+                // sample.
+                proceedWithUpdate = true;
+            }
+            else if (sampleIndex < SampleIndexOfValue)
+            {
+                // only update if incoming sample index precedes the existing sample
+                // in original submission.
+                proceedWithUpdate = true;
+            }
+
+            if (proceedWithUpdate)
             {
                 SampleIndexOfValue = sampleIndex;
                 Value = value;
