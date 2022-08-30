35f0e48ec81d1c21a8a3039fa4ab3d3a9b743bc8
Aaron Baffour-Awuah <aaronbaffourawuah@gmail.com>
2022-08-30 07:57:26 -0400

    Began testing classes in Mediator.Registry namespace.

---
+++ test/Kabomu.Tests/Mediator/Registry/EmptyRegistryTest.cs
@@ -0,0 +1,41 @@
+﻿using Kabomu.Mediator.Registry;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Kabomu.Tests.Mediator.Registry
+{
+    public class EmptyRegistryTest
+    {
+        [Fact]
+        public void TestTryGet()
+        {
+            ValueTuple<bool, object> expected = (false, null);
+            var actual = EmptyRegistry.Instance.TryGet("key");
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestGetAll()
+        {
+            var expected = new List<object>();
+            var actual = EmptyRegistry.Instance.GetAll("key");
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestTryGetFirst()
+        {
+            ValueTuple<bool, object> expected = (false, null);
+            var actual = EmptyRegistry.Instance.TryGetFirst("key", _ => (true, null));
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestGet()
+        {
+            Assert.Throws<RegistryException>(() => EmptyRegistry.Instance.Get("key"));
+        }
+    }
+}
---
+++ test/Kabomu.Tests/Mediator/Registry/TypeRegistryKeyPatternTest.cs
@@ -0,0 +1,45 @@
+﻿using Kabomu.Mediator.Registry;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Kabomu.Tests.Mediator.Registry
+{
+    public class TypeRegistryKeyPatternTest
+    {
+        [Fact]
+        public void TestErrorsInConstruction()
+        {
+            Assert.Throws<ArgumentNullException>(() => new TypeRegistryKeyPattern(null));
+        }
+
+        [Theory]
+        [MemberData(nameof(CreateTestIsMatchData))]
+        public void TestIsMatch(Type t, object input, bool expected)
+        {
+            var instance = new TypeRegistryKeyPattern(t);
+            var actual = instance.IsMatch(input);
+            Assert.Equal(expected, actual);
+        }
+
+        public static List<object[]> CreateTestIsMatchData()
+        {
+            return new List<object[]>
+            {
+                new object[]{ typeof(IRegistry), null, false },
+                new object[]{ typeof(IRegistry), 78, false },
+                new object[]{ typeof(IRegistry), typeof(string), false },
+                new object[]{ typeof(IRegistry), typeof(EmptyRegistry), true },
+                new object[]{ typeof(IRegistry), new TypeRegistryKeyPattern(typeof(EmptyRegistry)), true },
+                new object[]{ typeof(EmptyRegistry), typeof(DefaultMutableRegistry), false },
+                new object[]{ typeof(EmptyRegistry), typeof(EmptyRegistry), true },
+                new object[]{ typeof(EmptyRegistry), new TypeRegistryKeyPattern(typeof(EmptyRegistry)), true },
+                new object[]{ typeof(EmptyRegistry), new TypeRegistryKeyPattern(typeof(IRegistry)), false },
+                new object[]{ typeof(string), typeof(string), true },
+                new object[]{ typeof(string), 4, false },
+                new object[]{ typeof(string), new TypeRegistryKeyPattern(typeof(string)), true },
+            };
+        }
+    }
+}
