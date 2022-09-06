346fe81cb7d6bd3470a3b72e7e7fc97315115388
Aaron Baffour-Awuah <aaron.baffour-awuah@opiesoftware.com>
2022-09-06 10:16:56 -0400

    Added tests for constants declaring status codes and methods.

---
+++ test/Kabomu.Tests/QuasiHttp/DefaultQuasiHttpRequestTest.cs
@@ -0,0 +1,25 @@
+﻿using Kabomu.QuasiHttp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Kabomu.Tests.QuasiHttp
+{
+    public class DefaultQuasiHttpRequestTest
+    {
+        [Fact]
+        public void TestClassConstants()
+        {
+            Assert.Equal("CONNECT", DefaultQuasiHttpRequest.MethodConnect);
+            Assert.Equal("DELETE", DefaultQuasiHttpRequest.MethodDelete);
+            Assert.Equal("GET", DefaultQuasiHttpRequest.MethodGet);
+            Assert.Equal("HEAD", DefaultQuasiHttpRequest.MethodHead);
+            Assert.Equal("OPTIONS", DefaultQuasiHttpRequest.MethodOptions);
+            Assert.Equal("PATCH", DefaultQuasiHttpRequest.MethodPatch);
+            Assert.Equal("POST", DefaultQuasiHttpRequest.MethodPost);
+            Assert.Equal("PUT", DefaultQuasiHttpRequest.MethodPut);
+            Assert.Equal("TRACE", DefaultQuasiHttpRequest.MethodTrace);
+        }
+    }
+}
