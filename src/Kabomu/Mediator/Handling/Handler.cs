0953886f20351ba53ad2639f765456d178d01535
Aaron Baffour-Awuah <aaron.baffour-awuah@opiesoftware.com>
2022-09-12 15:06:54 -0400

    Began documenting classes in Kabomu.Mediator namespace.

--- src/Kabomu/Mediator/Handling/Handler.cs
+++ src/Kabomu/Mediator/Handling/Handler.cs
@@ -3,6 +3,20 @@ using System.Threading.Tasks;
 
 namespace Kabomu.Mediator.Handling
 {
+    /// <summary>
+    /// Represents a function that can process quasi http requests. All handlers are arranged in chain.
+    /// </summary>
+    /// <remarks>
+    /// A handler is expected to do at most one of 4 things:
+    /// <list type="bullet">
+    /// <item>call the next handler in line</item>
+    /// <item>insert new handlers and call the first of them</item>
+    /// <item>skip insert handler</item>
+    /// <item>call Send* methods to commit quasi http response</item>
+    /// </list>
+    /// </remarks>
+    /// <param name="context">quasi http context</param>
+    /// <returns>task representing asynchronus operation.</returns>
     public delegate Task Handler(IContext context);
 
     /// <summary>
--- src/Kabomu/Mediator/ResponseRendering/IResponseRenderer.cs
+++ src/Kabomu/Mediator/ResponseRendering/IResponseRenderer.cs
@@ -6,9 +6,29 @@ using System.Threading.Tasks;
 
 namespace Kabomu.Mediator.ResponseRendering
 {
+    /// <summary>
+    /// Represents functions that can serialize supported objects to quasi http context
+    /// responses.
+    /// </summary>
     public interface IResponseRenderer
     {
+        /// <summary>
+        /// Determines which objects that instances of this interface support for rendering or serialization
+        /// to quasi http context responses.
+        /// </summary>
+        /// <param name="context">quasi http context</param>
+        /// <param name="obj">object to test</param>
+        /// <returns>true if this instance can render/serialize the <paramref name="obj"/> argument; false
+        /// if the object argument cannot be serialized.</returns>
         bool CanRender(IContext context, object obj);
+
+        /// <summary>
+        /// Renders/serializes an object to a quasi http context response. Should fail if
+        /// object fails the test done by the <see cref="CanRender(IContext, object)"/> method.
+        /// </summary>
+        /// <param name="context">quasi http context</param>
+        /// <param name="obj">object to render.</param>
+        /// <returns>a task asynchronous operation</returns>
         Task Render(IContext context, object obj);
     }
 }
