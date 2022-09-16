7e3b4003e208ff8cfaeb93756c9e569e2a40de48
Aaron Baffour-Awuah <aaronbaffourawuah@gmail.com>
2022-09-16 17:10:54 -0400

    Began documenting Mediator.Handling namespace.

--- src/Kabomu/Mediator/Handling/IContextRequest.cs
+++ src/Kabomu/Mediator/Handling/IContextRequest.cs
@@ -7,13 +7,40 @@ using System.Text;
 
 namespace Kabomu.Mediator.Handling
 {
+    /// <summary>
+    /// Represents an association of a quasi http request with a mutable dictionary of request attributes.
+    /// </summary>
     public interface IContextRequest : IMutableRegistry
     {
+        /// <summary>
+        /// Gets the underlying quasi http request.
+        /// </summary>
         IQuasiHttpRequest RawRequest { get; }
+
+        /// <summary>
+        /// Gets the request environment supplied at request time.
+        /// </summary>
         IDictionary<string, object> Environment { get; }
+
+        /// <summary>
+        /// Gets the method of the <see cref="RawRequest"/> property.
+        /// </summary>
         string Method { get; }
+
+        /// <summary>
+        /// Gets the target of the <see cref="RawRequest"/> property.
+        /// </summary>
         string Target { get; }
+
+        /// <summary>
+        /// Returns an instance of <see cref="IHeadersWrapper"/> class that
+        /// provides convenient access to the headers in the <see cref="RawRequest"/> property.
+        /// </summary>
         IHeadersWrapper Headers { get; }
+
+        /// <summary>
+        /// Gets the body of the <see cref="RawRequest"/> property.
+        /// </summary>
         IQuasiHttpBody Body { get; }
     }
 }
